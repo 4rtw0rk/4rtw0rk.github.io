1bec6f45af608126fa7da87be299cf835df500a8
4rtw0rk <rahmat.artwork@gmail.com>
2022-10-23 22:18:31 +0800

    edit 5

---
+++ lat_python.py
@@ -0,0 +1,5 @@
+xsaya   = "Muhammad Aditya", 7, "Malang"
+print("Nama Saya :", xsaya[0])
+print(xsaya)
+#print("Panggilan Saya :", xsaya[9:15])
+#print("Umur Saya", xsaya[16], "dan Tinggal di Kota "xsaya[17:19])
