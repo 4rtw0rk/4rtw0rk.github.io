d5ba6130f64f5e99768e78e2fa178ff0bc878349
4rtw0rk <rahmat.artwork@gmail.com>
2022-10-24 18:26:56 +0800

    perulangan for

--- lat_python.py
+++ lat_python.py
@@ -1,9 +1,7 @@
-xorang = int(input("Masukkan Angkanya : "))
-if xorang in range(100, 900):
-    print("Angkanmu Besar")
-elif xorang in range (1000, 5000):
-    print(">>", xorang, ": Angkamu Besar Sekali")
-elif xorang < 100:
-    print(">>", xorang,": Angkamu Kecil")
-else:
-    print("Salah Input. Ulangi")
+xwarna = "merah", "kuning", "hijau"
+for xdaftar in range(len(xwarna)):
+    print("Saya Suka Warna ", xwarna[xdaftar])
+
+xwarna = "Pink", "Hitam", "Cokelat"
+for xdaftar in range(len(xwarna)):
+    print("Saya Tidak Suka Warna", xwarna[xdaftar])
