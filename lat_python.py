47fc016e10a730506df554cb0e77120c50ebaeb3
4rtw0rk <rahmat.artwork@gmail.com>
2022-10-24 09:14:38 +0800

    edit 8

--- lat_python.py
+++ lat_python.py
@@ -1,6 +1,8 @@
-xbulan = "Januari","Februari","Maret","April","Mei","Juni"," Juli","Agustus","September","Oktober","November","Desember"
-print("Pendaftaran Bulan",xbulan[9])
+xbulan = "Januari","Februari","Maret"
+print("Pendaftaran Bulan",xbulan[1])
 xnama = input("Nama : ")
 xumur = input("Umur : ")
 xkota = input("Kota : ")
-print(">> Server << : Pendaftaran Di bulan" ,xbulan[9],"Nama ", xnama,", Umur", xumur, "Tahun , Asal Kota", xkota, "Berhasil dibuat. Terima Kasih")
+print(">> Pendaftaran Di bulan" ,xbulan[1],
+    "\n>> Nama ", xnama,", Umur", xumur, "Tahun , Asal Kota", xkota,
+    "\n>> Berhasil dibuat. Terima Kasih")
