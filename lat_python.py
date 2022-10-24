c280fd10d814b3f52c9b301e0e035941d0a927e6
4rtw0rk <rahmat.artwork@gmail.com>
2022-10-24 14:28:26 +0800

    percabangan python

--- lat_python.py
+++ lat_python.py
@@ -1,8 +1,9 @@
-xbulan = "Januari","Februari","Maret"
-print("Pendaftaran Bulan",xbulan[1])
-xnama = input("Nama : ")
-xumur = input("Umur : ")
-xkota = input("Kota : ")
-print(">> Pendaftaran Di bulan" ,xbulan[1],
-    "\n>> Nama ", xnama,", Umur", xumur, "Tahun , Asal Kota", xkota,
-    "\n>> Berhasil dibuat. Terima Kasih")
+xorang = int(input("Masukkan Angkanya : "))
+if xorang in range(100, 900):
+    print("Angkanmu Besar")
+elif xorang in range (1000, 5000):
+    print(">>", xorang, ": Angkamu Besar Sekali")
+elif xorang < 100:
+    print(">>", xorang,": Angkamu Kecil")
+else:
+    print("Salah Input. Ulangi")
